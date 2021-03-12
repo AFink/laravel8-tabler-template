065c2080c4e31248c2534d6119a03f690f75efa2
Andreas Fink <andreas@andreasfink.xyz>
2021-03-12 07:15:18 +0100

    move style to body for livewire and remove font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,43 +1,43 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
-        @livewireStyles
+    @livewireStyles
+</head>
 
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased bg-light">
-        <x-jet-banner />
-        @livewire('navigation-menu')
+<body class="font-sans antialiased bg-light">
+    <x-jet-banner />
+    @livewire('navigation-menu')
 
-        <!-- Page Heading -->
-        <header class="d-flex py-3 bg-white shadow-sm border-bottom">
-            <div class="container">
-                {{ $header }}
-            </div>
-        </header>
+    <!-- Page Heading -->
+    <header class="d-flex py-3 bg-white shadow-sm border-bottom">
+        <div class="container">
+            {{ $header }}
+        </div>
+    </header>
 
-        <!-- Page Content -->
-        <main class="container my-5">
-            {{ $slot }}
-        </main>
+    <!-- Page Content -->
+    <main class="container my-5">
+        {{ $slot }}
+    </main>
 
-        @stack('modals')
+    @stack('modals')
 
-        @livewireScripts
+    <!-- Scripts -->
+    @livewireScripts
 
-        @stack('scripts')
-    </body>
+    <script src="{{ mix('js/app.js') }}" defer></script>
+
+    @stack('scripts')
+</body>
+
 </html>
