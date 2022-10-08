9674c2574b589cbc7d9f9f0980ea6746e372233b
fbrettnich <github@fbrettnich.de>
2022-10-08 13:25:48 +0200

    Updated tabler-icons to afink/blade-tablericons
    Signed-off-by: fbrettnich <github@fbrettnich.de>

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -27,7 +27,7 @@
                     <div class="card-body border-right border-bottom p-3 h-100">
                         <div class="d-flex flex-row bd-highlight pt-2">
                             <div>
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="text-muted" width="32"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
+                                <x-tablericon-book class="text-muted" width="32" />
                             </div>
                             <div class="pl-3">
                                 <h3 class="card-title font-weight-bolder text-decoration-none text-dark my-1">
@@ -44,7 +44,7 @@
                     <div class="card-body border-bottom p-3 h-100">
                         <div class="d-flex flex-row bd-highlight pt-2">
                             <div>
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="text-muted" width="32"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+                                <x-tablericon-camera class="text-muted" width="32" />
                             </div>
                             <div class="pl-3">
                                 <h3 class="card-title font-weight-bolder text-decoration-none text-dark my-1">
@@ -61,7 +61,7 @@
                     <div class="card-body border-right p-3 h-100">
                         <div class="d-flex flex-row bd-highlight pt-2">
                             <div>
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="text-muted" width="32"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
+                                <x-tablericon-message-2 class="text-muted" width="32" />
                             </div>
                             <div class="pl-3 text-sm">
                                 <h3 class="card-title font-weight-bolder text-decoration-none text-dark my-1">
@@ -78,7 +78,7 @@
                     <div class="card-body p-3 h-100">
                         <div class="d-flex flex-row bd-highlight pt-2">
                             <div>
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="text-muted" width="32"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                <x-tablericon-world class="text-muted" width="32" />
                             </div>
                             <div class="pl-3">
                                 <h3 class="card-title font-weight-bolder text-decoration-none text-dark my-1">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,7 +56,7 @@
                             <div class="card-body border-right border-bottom p-3 h-100">
                                 <div class="d-flex flex-row bd-highlight pt-2">
                                     <div>
-                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="text-muted" width="32"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
+                                        <x-tablericon-book class="text-muted" width="32" />
                                     </div>
                                     <div class="pl-3">
                                         <h3 class="card-title font-weight-bolder text-decoration-none text-dark my-1">
@@ -73,7 +73,7 @@
                             <div class="card-body border-bottom p-3 h-100">
                                 <div class="d-flex flex-row bd-highlight pt-2">
                                     <div>
-                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="text-muted" width="32"><path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"></path><path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+                                        <x-tablericon-camera class="text-muted" width="32" />
                                     </div>
                                     <div class="pl-3">
                                         <h3 class="card-title font-weight-bolder text-decoration-none text-dark my-1">
@@ -90,7 +90,7 @@
                             <div class="card-body border-right p-3 h-100">
                                 <div class="d-flex flex-row bd-highlight pt-2">
                                     <div>
-                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="text-muted" width="32"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
+                                        <x-tablericon-message-2 class="text-muted" width="32" />
                                     </div>
                                     <div class="pl-3 text-sm">
                                         <h3 class="card-title font-weight-bolder text-decoration-none text-dark my-1">
@@ -107,7 +107,7 @@
                             <div class="card-body p-3 h-100">
                                 <div class="d-flex flex-row bd-highlight pt-2">
                                     <div>
-                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="text-muted" width="32"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                        <x-tablericon-world class="text-muted" width="32" />
                                     </div>
                                     <div class="pl-3">
                                         <h3 class="card-title font-weight-bolder text-decoration-none text-dark my-1">
@@ -126,18 +126,12 @@
                 <div class="d-flex justify-content-between mt-3">
                     <div class="text-sm text-muted">
                         <div class="flex align-content-center">
-                            <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor" class="text-muted" style="width: 18px">
-                                <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
-                            </svg>
-
+                            <x-tablericon-shopping-cart class="ml-4 text-muted" style="width: 18px" />
                             <a href="https://laravel.bigcartel.com" class="text-muted">
                                 {{ __('Shop') }}
                             </a>
 
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="ml-4 text-muted" style="width: 18px">
-                                <path d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path>
-                            </svg>
-
+                            <x-tablericon-heart class="ml-4 text-muted" style="width: 18px" />
                             <a href="https://github.com/sponsors/taylorotwell" class="text-muted">
                                 {{ __('Sponsor') }}
                             </a>
